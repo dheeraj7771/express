8a3693536829331b24c533a7ea0046374ad0933a
dheeraj <dheeraj.07april@gmail.com>
2018-02-04 11:42:33 -0500

    dynamic port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+var PORT = process.env.PORT;
 var express = require('express');
 var app = express();
 
-app.listen(3000, function(){
-    console.log("server is up and running n port 3000");
+app.listen(PORT, function(){
+    console.log("server is up and running n port:- "+ PORT);
 });
 
 app.get('/', function(request, response){
