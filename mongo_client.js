5e6403f06c790164ca525375e022cc58d6f28296
dheeraj <dheeraj.07april@gmail.com>
2018-02-04 13:54:24 -0500

    db connections

--- mongo_client.js
+++ mongo_client.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-const MONGO_URI = process.env.MONGO_URI;
+// require('dotenv').config();
+// const MongoClient = require('mongodb').MongoClient;
+// const MONGO_URI = process.env.MONGO_URI;
 
-app.get('/users', function(request, response){
-    MongoClient.connect(MONGO_URI, function(err, db){
-        if(err) {
-            response.send(`Error fetching all users => ${err}`);
-        }
-        db.collection('users').find().toArray(function(errdb, docs) {
-            if(errdb){
-                response.send(`Error fetching all users => ${errdb}`);
-            }
-            response.send(docs);
-        });
-    });
-});
+// app.get('/users', function(request, response){
+//     MongoClient.connect(MONGO_URI, function(err, db){
+//         if(err) {
+//             response.send(`Error fetching all users => ${err}`);
+//         }
+//         db.collection('users').find().toArray(function(errdb, docs) {
+//             if(errdb){
+//                 response.send(`Error fetching all users => ${errdb}`);
+//             }
+//             response.send(docs);
+//         });
+//     });
+// });
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 var PORT = process.env.PORT;
 var express = require('express');
 var app = express();
+const MongoClient = require('mongodb').MongoClient;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.listen(PORT, function(){
     console.log("server is up and running n port:- "+ PORT);
@@ -9,4 +11,18 @@ app.listen(PORT, function(){
 
 app.get('/', function(request, response){
     response.send("Hello World by dheeraj");
+});
+
+app.get('/users', function(request, response){
+    MongoClient.connect(MONGO_URI, function(err, db){
+        if(err) {
+            response.send(`Error fetching all users => ${err}`);
+        }
+        db.collection('users').find().toArray(function(errdb, docs) {
+            if(errdb){
+                response.send(`Error fetching all users => ${errdb}`);
+            }
+            response.send(docs);
+        });
+    });
 });
